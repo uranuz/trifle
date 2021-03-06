b9de119cd5ddbf4c2e76ea24a3c0dd1afa0695e1
uranuz <neuranuz@gmail.com>
2021-03-06 10:25:51 +0300

    Location stuff simlified

--- src/trifle/location.d
+++ src/trifle/location.d
@@ -1,51 +1,6 @@
 module trifle.location;
 
-struct LocationConfig
-{
-	bool withGraphemeIndex = true;
-	bool withLineIndex = true;
-	bool withColumnIndex = true;
-	bool withGraphemeColumnIndex = true;
-	bool withSize = true;
-}
-
-enum IndentStyle: char {
-	none = 0,
-	tab = '\t',
-	space = ' '
-}
-
 struct Location
-{
-	string fileName;  // Name of source file
-	size_t index;     // Start code unit index or grapheme index (if available)
-	size_t length;    // Length of source text in code units or in graphemes (if available)
-	size_t indentCount;
-	IndentStyle indentStyle;
-
-	string toString()
-	{
-		import std.conv: text;
-
-		return fileName ~ ` [` ~ index.text ~ ` .. ` ~ (index + length).text ~ `]`;
-	}
-}
-
-struct PlainLocation
-{
-	string fileName;
-	size_t lineIndex;
-	size_t columnIndex;
-
-	string toString()
-	{
-		import std.conv: text;
-
-		return fileName ~ `:` ~ lineIndex.text ~ `:` ~ columnIndex.text;
-	}
-}
-
-struct ExtendedLocation
 {
 	string fileName; // File name for this source
 	size_t index; // Index of UTF code unit that starts element
@@ -56,122 +11,11 @@ struct ExtendedLocation
 	size_t lineCount; // Number of lines in element (number of CR LF/ CR / LF exactly)
 	size_t columnIndex; // Index of code unit in line that starts element
 	size_t graphemeColumnIndex; // Index of grapheme in line that starts element
-	size_t indentCount; // Line indent count for element
-	IndentStyle indentStyle; // Determines if element indented with tabs or spaces
 
 	string toString()
 	{
 		import std.conv: text;
 
 		return fileName ~ `:` ~ lineIndex.text ~ `:` ~ graphemeColumnIndex.text;
-	}
-}
-
-struct CustomizedLocation(LocationConfig c)
-{
-	enum config = c;
-
-	string fileName;
-
-	size_t index;
-
-	static if( config.withSize )
-		size_t length;
-
-	static if( config.withGraphemeIndex )
-	{
-		size_t graphemeIndex;
-
-		static if( config.withSize )
-			size_t graphemeLength;
-	}
-
-	static if( config.withLineIndex )
-	{
-		size_t lineIndex;
-
-		static if( config.withSize )
-			size_t lineCount;
-
-		static if( config.withColumnIndex )
-			size_t columnIndex;
-
-		static if( config.withGraphemeColumnIndex )
-			size_t graphemeColumnIndex;
-	}
-
-	size_t indentCount;
-	IndentStyle indentStyle;
-
-	Location toLocation() const
-	{
-		Location loc;
-		loc.fileName = fileName;
-		loc.index = index;
-		loc.indentCount = indentCount;
-		loc.indentStyle = indentStyle;
-
-		static if( config.withSize )
-			loc.length = length;
-
-		return loc;
-	}
-
-	PlainLocation toPlainLocation() const
-	{
-		PlainLocation loc;
-		loc.fileName = fileName;
-
-		static if( config.withLineIndex )
-			loc.lineIndex = lineIndex;
-
-		static if( config.withLineIndex && config.withGraphemeColumnIndex )
-			loc.columnIndex = graphemeColumnIndex;
-		else static if( config.withLineIndex && config.withColumnIndex )
-			loc.columnIndex = columnIndex;
-		else static if( config.withGraphemeIndex )
-			loc.columnIndex = graphemeIndex;
-		else
-			loc.columnIndex = index;
-
-		return loc;
-	}
-
-	ExtendedLocation toExtendedLocation() const
-	{
-		ExtendedLocation loc;
-
-		loc.fileName = fileName;
-		loc.index = index;
-
-		static if( config.withSize )
-			loc.length = length;
-
-		static if( config.withGraphemeIndex )
-		{
-			loc.graphemeIndex = graphemeIndex;
-
-			static if( config.withSize )
-				loc.graphemeLength = graphemeLength;
-		}
-
-		static if( config.withLineIndex )
-		{
-			loc.lineIndex = lineIndex;
-
-			static if( config.withSize )
-				loc.lineCount = lineCount;
-
-			static if( config.withColumnIndex )
-				loc.columnIndex = columnIndex;
-
-			static if( config.withGraphemeColumnIndex )
-				loc.graphemeColumnIndex = graphemeColumnIndex;
-		}
-
-		loc.indentCount = indentCount;
-		loc.indentStyle = indentStyle;
-
-		return loc;
 	}
 }
--- src/trifle/text_forward_range.d
+++ src/trifle/text_forward_range.d
@@ -1,20 +1,18 @@
 module trifle.text_forward_range;
 
-import trifle.location: LocationConfig, IndentStyle;
 import trifle.parse_utils: isStartCodeUnit, frontUnitLength;
 
 import std.traits: isSomeString, Unqual;
 
-struct TextForwardRange(S, LocationConfig c = LocationConfig.init)
+struct TextForwardRange(S)
 	if( isSomeString!S )
 {
 	import std.range: ElementEncodingType;
 
 	alias String = S;
 	alias Char = Unqual!(ElementEncodingType!S);
-	enum LocationConfig config = c;
 
-	alias ThisType = TextForwardRange!(S, c);
+	alias ThisType = TextForwardRange!(S);
 
 	// This range keeps original reference to data internally
 	String str;
@@ -24,24 +22,14 @@ struct TextForwardRange(S, LocationConfig c = LocationConfig.init)
 	// Index of tail of this range or size_t.max if range ends at end of original data buffer
 	size_t endIndex = size_t.max;
 
-	static if( config.withGraphemeIndex )
-		size_t graphemeIndex = 0;
+	size_t graphemeIndex = 0;
 
-	static if( config.withLineIndex )
-	{
-		size_t lineIndex = 0; // Line index related to original data buffer
+	// Line index related to original data buffer
+	size_t lineIndex = 0; 
 
-		static if( config.withColumnIndex )
-			size_t columnIndex = 0;
+	size_t columnIndex = 0;
 
-		static if( config.withGraphemeColumnIndex )
-			size_t graphemeColumnIndex = 0;
-	}
-
-	// If it set to true means that indents on current line have passed
-	private bool isIndenting = true;
-	IndentStyle indentStyle; // Tabs or spaces indent style for current line
-	size_t indentCount; // Indentation count in number of tabs or spaces
+	size_t graphemeColumnIndex = 0;
 
 
 public:
@@ -50,8 +38,6 @@ public:
 	this( String source )
 	{
 		str = source;
-
-		analyzeIndents();
 	}
 
 	// copy ctor
@@ -63,23 +49,11 @@ public:
 
 		this.index = rhs.index;
 		this.endIndex = rhs.endIndex;
-		this.isIndenting = rhs.isIndenting;
-		this.indentCount = rhs.indentCount;
-		this.indentStyle = rhs.indentStyle;
 
-		static if( config.withGraphemeIndex )
-			this.graphemeIndex = rhs.graphemeIndex;
-
-		static if( config.withLineIndex )
-		{
-			this.lineIndex = rhs.lineIndex;
-
-			static if( config.withColumnIndex )
-				this.columnIndex = rhs.columnIndex;
-
-			static if( config.withGraphemeColumnIndex )
-				this.graphemeColumnIndex = rhs.graphemeColumnIndex;
-		}
+		this.graphemeIndex = rhs.graphemeIndex;
+		this.lineIndex = rhs.lineIndex;
+		this.columnIndex = rhs.columnIndex;
+		this.graphemeColumnIndex = rhs.graphemeColumnIndex;
 	}
 
 	/// Test if current range is empty or fully consumed
@@ -108,45 +82,6 @@ public:
 		return ch;
 	}
 
-	// Tests if current item is line indentation symbol
-	private void analyzeIndents()
-	{
-		if( index >= this.sliceEndIndex )
-			return;
-
-		if( isNewLine )
-		{
-			indentCount = 0;
-			isIndenting = true;
-		}
-
-		if( isIndenting )
-		{
-			if( indentCount == 0 )
-			{
-				if( str[index] == '\t' )
-				{
-					indentStyle = IndentStyle.tab;
-					++indentCount;
-				}
-				else if( str[index] == ' ' )
-				{
-					indentStyle = IndentStyle.space;
-					++indentCount;
-				}
-				else
-					isIndenting = false;
-			}
-			else
-			{
-				if( str[index] == indentStyle )
-					++indentCount;
-				else
-					isIndenting = false;
-			}
-		}
-	}
-
 	/// Push the range forward by one encoding element
 	void popFront()
 	{
@@ -155,24 +90,14 @@ public:
 		if( index >= this.sliceEndIndex )
 			return;
 
-		analyzeIndents();
-
-		static if( config.withLineIndex )
+		if( isNewLine )
 		{
-			if( isNewLine )
-			{
-				lineIndex++;
-
-				static if( config.withColumnIndex )
-					columnIndex = 0;
-
-				static if( config.withGraphemeColumnIndex )
-					graphemeColumnIndex = 0;
-			}
-
-			static if( config.withColumnIndex )
-				columnIndex++;
+			lineIndex++;
+			columnIndex = 0;
+			graphemeColumnIndex = 0;
 		}
+
+		columnIndex++;
 
 		import std.traits: Unqual;
 
@@ -180,20 +105,14 @@ public:
 		{
 			if( isStartCodeUnit(str[index]) )
 			{
-				static if( config.withGraphemeIndex )
-					graphemeIndex++;
-
-				static if( config.withGraphemeColumnIndex )
-					graphemeColumnIndex++;
+				graphemeIndex++;
+				graphemeColumnIndex++;
 			}
 		}
 		else static if( is( Char == dchar ) )
 		{
-			static if( config.withGraphemeIndex )
-				graphemeIndex++;
-
-			static if( config.withGraphemeColumnIndex )
-				graphemeColumnIndex++;
+			graphemeIndex++;
+			graphemeColumnIndex++;
 		}
 		else
 			static assert( false, "Code unit type '" ~ Char.stringof ~ "' is not valid!" );
@@ -216,48 +135,6 @@ public:
 		return index == 0 || str[index-1] == '\n' || ( str[index-1] == '\r' && str[index] != '\n' );
 	}
 
-	/// Tests if range front currently points to indentation symbol
-	@property bool isIndentation() {
-		return isIndenting && ( str[index] == '\t' || str[index] == ' ' );
-	}
-
-	/// Get current's line indent info without pushing this range forward
-	void getLineIndent( ref size_t count, ref IndentStyle style )
-	{
-		bool isInd = this.isIndentation;
-
-		if( !isInd )
-		{
-			count = indentCount;
-			style = indentStyle;
-			return;
-		}
-
-		auto tmp = this.save;
-		isInd = tmp.isIndentation;
-
-		while( isInd && !tmp.empty )
-		{
-			tmp.popFront();
-			isInd = tmp.isIndentation;
-		}
-
-		count = tmp.indentCount;
-		style = tmp.indentStyle;
-
-		return;
-	}
-
-	/// Parses current's line indent with pushing the range forward
-	void parseLineIndent( ref size_t count, ref IndentStyle style )
-	{
-		while( isIndentation && !this.empty )
-			this.popFront();
-
-		count = indentCount;
-		style = indentStyle;
-	}
-
 	bool match(String input)
 	{
 		import std.algorithm: equal;
@@ -272,58 +149,6 @@ public:
 			popFront();
 
 		return true;
-	}
-
-	bool matchWord(String input)
-	{
-		import std.uni: isAlpha;
-		import std.algorithm: equal;
-
-		import trifle.parse_utils: decodeFront;
-
-		size_t inpIndex = 0;
-
-		auto thisSlice = this.save; //Creating temporary slice of this range
-
-		if( input.length > thisSlice.length || input.length == 0 || thisSlice.empty )
-			return false;
-
-		Char thisChar = thisSlice.front;
-		dchar thisDChar;
-		ubyte codeUnitLen;
-
-		//Match if we have input starting with
-		if( !isStartCodeUnit(input[0]) || !isStartCodeUnit(thisChar) )
-			return false;
-
-		while( !thisSlice.empty )
-		{
-			thisDChar = thisSlice.decodeFront();
-
-			codeUnitLen = frontUnitLength(thisSlice);
-
-			if( isAlpha(thisDChar) )
-			{
-				if( inpIndex + codeUnitLen > input.length )
-					return false;
-
-				if( !equal( thisSlice[0 .. codeUnitLen], input[inpIndex .. inpIndex + codeUnitLen] ) )
-					return false;
-			}
-			else
-				break;
-
-			inpIndex += codeUnitLen;
-			thisSlice.popFrontN(codeUnitLen);
-		}
-
-		if( thisSlice.empty || ( !isAlpha(thisDChar) && ( inpIndex + codeUnitLen > input.length ) ) )
-		{
-			this = thisSlice.save;
-			return true;
-		}
-		else
-			return false;
 	}
 
 	auto opSlice() const {
@@ -359,21 +184,5 @@ public:
 
 	string toString() inout {
 		return str[index .. this.sliceEndIndex];
-	}
-}
-
-template GetSourceRangeConfig(R)
-{
-	static if( is( typeof(R.config) == LocationConfig ) )
-	{
-		enum GetSourceRangeConfig = R.config;
-	}
-	else
-	{
-		enum GetSourceRangeConfig = ( () {
-			LocationConfig c;
-
-			return c;
-		} )();
 	}
 }
